27f5ca2330100f2687709be4bdce126888fdf627
Aaron Williams <856628+aboutaaron@users.noreply.github.com>
2022-06-13 16:06:17 -0700

    feat: add flyTo pitch/rotate animations to map

--- src/components/Mapbox.svelte
+++ src/components/Mapbox.svelte
@@ -7,29 +7,44 @@
 
   let container;
   let map;
+  let flying = false;
 
   // set mapbox config detials based on slide index
   const mapboxConfig = new Map();
   mapboxConfig.set("default", {
     center: [-122.257426, 37.829682],
-    zoom: 10 // 0 = earth, 3 = continent, 10 = roads, 15 = buildings
+    zoom: 10, // 0 = earth, 3 = continent, 10 = roads, 15 = buildings
+    pitch: 0
   });
   mapboxConfig.set("gardena", {
     center: [-118.2998, 33.8818],
-    zoom: 10
+    zoom: 10,
+    pitch: 40
   });
   mapboxConfig.set("sandel", {
-    center: [-118.28581, 33.88793],
-    zoom: 14
+    center: [-118.270405, 33.8899148],
+    zoom: 18,
+    pitch: 50
   });
   mapboxConfig.set("playa-del-rey", {
     center: [-118.43411, 33.95663],
-    zoom: 14
+    zoom: 14,
+    pitch: 60
   });
   mapboxConfig.set("fremont", {
     center: [-122.001, 37.5306],
-    zoom: 11
+    zoom: 11,
+    pitch: 0
   });
+
+  const rotateCamera = (timestamp) => {
+    if (!map) return;
+    // clamp the rotation between 0 -360 degrees
+    // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+    map.rotateTo((timestamp / 100) % 360, { duration: 0 });
+    // Request the next frame of the animation.
+    requestAnimationFrame(rotateCamera);
+  };
 
   const load = () => {
     map = new mapbox.Map({
@@ -59,6 +74,15 @@
     if (!hasId || index === undefined) return null;
 
     map.flyTo(mapboxConfig.get(slide.id));
+    map.once("moveend", () => {
+      if (slide.id === "sandel") {
+        map.rotateTo(80, { duration: 6500 });
+      } else if (slide.id === "playa-del-rey") {
+        map.rotateTo(-120, { duration: 6500 });
+      } else {
+        map.rotateTo(0, { duration: 2500 });
+      }
+    });
   });
 </script>
 
