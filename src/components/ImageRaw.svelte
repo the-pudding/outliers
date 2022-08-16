5510bbeea466559e92b9dddc2d899839071746ef
Aaron Williams <856628+aboutaaron@users.noreply.github.com>
2022-08-16 05:02:13 -0700

    fix: add temp URL path to ImageRaw

--- src/components/ImageRaw.svelte
+++ src/components/ImageRaw.svelte
@@ -4,7 +4,7 @@
     const isDev = process.env.NODE_ENV === "development";
     const placeholder = "https://via.placeholder.com/800x300";
   
-    const rootpath = isDev ? "/" : "/tbd/"
+    const rootpath = isDev ? "/" : "/outliers/"
   
     export let id;
     export let src
