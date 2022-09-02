fe1f88e99d30321b3126334246e463a59c2fc188
Aaron Williams <856628+aboutaaron@users.noreply.github.com>
2022-09-01 17:18:26 -0700

    feat: second pass at topper

--- src/components/Mobility.svelte
+++ src/components/Mobility.svelte
@@ -1,4 +1,6 @@
 <script>
+  import ImageRaw from "$components/ImageRaw.svelte";
+
   import { beforeUpdate, onMount } from "svelte";
 
   import { feature } from "topojson-client";
@@ -9,17 +11,13 @@
   import { zoom as d3_zoom, zoomIdentity, zoomTransform } from "d3-zoom"
 
   import copy from "$data/doc.json";
-  import mobilityTopoJson from "$data/geo/mobility-topo.json"
+  import usaTopoJson from "$data/geo/us-topo.json"
+import { scaleBand } from "d3";
   // ------------------------
-
-  const GARDENA_GEO_ID = "06037541001"
-  const FREMONT_GEO_ID = "06001441924"
-
-  const WIDTH = 500
-  const HEIGHT = 960
 
   let svg;
   let g
+  let timelineDiv
 
   const zoomed = ({transform}) => {
     const _g = select(g)
@@ -28,118 +26,122 @@
   }
   
   const zoom = d3_zoom().scaleExtent([1, 14]).on("zoom", zoomed)
+  const usaFeatureCollection = feature(usaTopoJson, usaTopoJson.objects.states)
+  // drop Alaska, Hawaii && PR
+  const states = usaFeatureCollection.features.filter(d => ![2, 15, 72].includes(d.id))
 
-  const losAngelesHoods = feature(mobilityTopoJson, mobilityTopoJson.objects.la)
-
-  const hoods = losAngelesHoods.features.filter((d) =>
-    ["Gardena", "West Compton"].includes(d.properties.Name)
-  );
-
-  const tracts = feature(mobilityTopoJson, mobilityTopoJson.objects.tracts)
-
-  const laTracts = tracts.features.filter(d => d.properties.county === "037")
-
-  const gardenaTract = laTracts.find(d => d.properties.GEOID10 === GARDENA_GEO_ID)
-  const fremontTract = laTracts.find(d => d.properties.GEOID10 === FREMONT_GEO_ID)
+  const homes = ["Madera", "Gardena", "Fremont"]
 
   let projection = geoAlbers()
-    .fitSize([WIDTH, WIDTH], featureCollection(laTracts));
+    .fitSize([960, 500], featureCollection(states));
 
   let path = geoPath().projection(projection);
 
   const resetZoom = (event) => {
-    if (svg === undefined) return null
-    select(svg).transition().duration(750)
-      .call(
-        zoom.transform,
-        zoomIdentity,
-        zoomTransform(svg).invert([WIDTH / 2, WIDTH / 2])
-      )
+    // if (svg === undefined) return null
+    // select(svg).transition().duration(750)
+    //   .call(
+    //     zoom.transform,
+    //     zoomIdentity,
+    //     zoomTransform(svg).invert([WIDTH / 2, WIDTH / 2])
+    //   )
   }
 
   const zoomOnClick = (event) => {
-    const [[x0, y0], [x1, y1]] = path.bounds(gardenaTract)
+    // const [[x0, y0], [x1, y1]] = path.bounds(gardenaTract)
 
-    const scale = Math.min(8, 0.9 / Math.max((x1 - x0) / WIDTH, (y1-y0) / WIDTH))
-    const translateX = -(x0 + x1) / 2
-    const translateY = -(y0 + y1) / 2
+    // const scale = Math.min(8, 0.9 / Math.max((x1 - x0) / WIDTH, (y1-y0) / WIDTH))
+    // const translateX = -(x0 + x1) / 2
+    // const translateY = -(y0 + y1) / 2
 
-    if (svg === undefined) return null
-    const _svg = select(svg)
-    _svg.transition()
-      .duration(750)
-      .call(
-        zoom.transform,
-        zoomIdentity
-          .translate(WIDTH / 2, WIDTH / 2)
-          .scale(scale)
-          .translate(translateX, translateY)
-      )
+    // if (svg === undefined) return null
+    // const _svg = select(svg)
+    // _svg.transition()
+    //   .duration(750)
+    //   .call(
+    //     zoom.transform,
+    //     zoomIdentity
+    //       .translate(WIDTH / 2, WIDTH / 2)
+    //       .scale(scale)
+    //       .translate(translateX, translateY)
+    //   )
 
-    _svg.select(`[data-geoid="${GARDENA_GEO_ID}"]`)
-      .attr("class", "fill-pink-500 opacity-7")
+    // _svg.select(`[data-geoid="${GARDENA_GEO_ID}"]`)
+    //   .attr("class", "fill-pink-500 opacity-7")
     
 
   }
 
   onMount(async () => {
     select(svg).call(zoom).on("wheel.zoom", null);
+
+    // setup timeline?
+    // const timeline = select(timelineDiv) s
+    // const dimensions = timeline.node().getBoundingClientRect()
+    
+    // const { width, height } = dimensions
+    // const scale = scaleBand().domain(["Madera, Gardena, Fremont"]).range([height, 0])
+
+
+
   });
 
   beforeUpdate(async () => {
     // TODO: toggle graphic changes
   });
+
+  const photoSrcList = ["madera", "gardena", "gardena2"]
+  const photoPositionClasses = {
+    madera: "top-[25%] rotate-3",
+    gardena: "top-[10%] -rotate-12",
+    gardena2: "top-[25%] rotate-6",
+  }
 </script>
 
-<div class="w-full h-screen relative z-0">
-  <div>
-    <button
-      on:click={zoomOnClick}
-      type="button"
-      class="label relative inline-flex items-center m-2 px-4 py-2 rounded-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 active:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-      >Click to zoom</button
-    >
-    <button
-      on:click={resetZoom}
-      type="button"
-      class="label relative inline-flex items-center m-2 px-4 py-2 rounded-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 active:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-      >Click to reset</button
-    >
+<div class="w-full h-screen relative z-[60] p-4 rotat">
+  <section class="grid custom-cols h-full max-w-7xl my-0 mx-auto">
+    <div bind:this={timelineDiv} class="flex flex-col justify-between h-full items-end">
+      {#each homes as home}
+        <div class="text-label">
+          <div class="flex gap-1">
+            <div class="flex flex-col items-end">
+              <p>{home}</p>
+              <p>1996</p>
+            </div>
+            <span class="text-hero">○</span>
+          </div>
+        </div>
+        {#if home !== "Fremont"}
+          <div class="w-[2px] m-1 h-2/3 bg-hero"></div>
+        {/if}
 
-  </div>
-
-  <div class="w-full flex ">
-    <div class="border-red-600 border-2 flex-initial w-1/3">
-      <svg class="" bind:this={svg} viewBox={`0 0 ${WIDTH} ${WIDTH}`}>
-        <g bind:this={g}>
-          <g id="los-angeles">
-            {#each laTracts as tract}
-              <path
-                data-geoid={`${tract.properties.GEOID10}`}
-                d={path(tract)}
-                class="fill-slate-200 stroke-white"
-                stroke-width={0.5}
-              />
-            {/each}
-          </g>
-          <g id="los-angeles-hoods">
-            {#each hoods as hood}
-            <path
-              d={path(hood)}
-              class="fill-red-600 opacity-50"
-              stroke-width={0.5}
-            />
-            {/each}
-          </g>
+      {/each}
+    </div>
     
-        </g>
-      </svg>
+    <div class="relative w-4/6 my-0 mx-auto p-10">
+      {#each photoSrcList as src, index}
+        <div class={`absolute ${photoPositionClasses[src]}`}>
+          <ImageRaw src={`assets/img/${src}.jpg`} />
+        </div>
+      {/each}
     </div>
-    <div class="border-black border-2 flex-initial w-2/3">
-      Chart stuff?
-    </div>
-  </div>
+    
 
+    <div>
+      stats?
+    </div>
+  </section>
 </div>
 
-<style></style>
+<style>
+  .custom-cols {
+    grid-template-columns: 0.5fr 4fr 0.5fr;
+  }
+
+  .text-hero {
+    color: var(--color-hero);
+  }
+  .bg-hero {
+    background-color: var(--color-hero);
+  }
+</style>
