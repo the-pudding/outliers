fc5d59198416e3b56ecc972484cc26a253950976
Michelle McGhee <michmac202@gmail.com>
2022-11-22 09:41:49 -0700

    fix links

--- src/components/ImageRaw.svelte
+++ src/components/ImageRaw.svelte
@@ -1,26 +1,23 @@
 <script>
-    import loadImage from "$utils/loadImage";
-  
-    const isDev = process.env.NODE_ENV === "development";
-    const placeholder = "https://via.placeholder.com/800x300";
-  
-    const rootpath = isDev ? "/" : "/outliers/"
-  
-    export let id;
-    export let src
-    export let alt;
-    export let className
-    export let width
-    export let height
-  </script>
-  
-  
-    {#await loadImage(`${rootpath}${src}`)}
-      <span class={className} {width} {height} />
-    {:then img}
-      <img class={className} {width} {height} {id} src={img.src} {alt} />
-    {:catch}
-      <img class={className} {width} {height} {id} src={placeholder} alt="placeholder" />
-    {/await}
-  
-  
+  import loadImage from "$utils/loadImage";
+
+  const isDev = process.env.NODE_ENV === "development";
+  const placeholder = "https://via.placeholder.com/800x300";
+
+  const rootpath = isDev ? "/" : "/2022/11/upward-mobility/";
+
+  export let id;
+  export let src;
+  export let alt;
+  export let className;
+  export let width;
+  export let height;
+</script>
+
+{#await loadImage(`${rootpath}${src}`)}
+  <span class={className} {width} {height} />
+{:then img}
+  <img class={className} {width} {height} {id} src={img.src} {alt} />
+{:catch}
+  <img class={className} {width} {height} {id} src={placeholder} alt="placeholder" />
+{/await}
--- src/components/StreetSvg.svelte
+++ src/components/StreetSvg.svelte
@@ -9,7 +9,7 @@
   const isDev = process.env.NODE_ENV === "development";
   const placeholder = "https://via.placeholder.com/800x300";
 
-  const rootpath = isDev ? "/" : "/outliers/"
+  const rootpath = isDev ? "/" : "/2022/11/upward-mobility/";
   export let step;
 
   let maderaPaths;
@@ -31,16 +31,16 @@
         .ease(easeLinear)
         .attr("stroke-dashoffset", 0)
         .on("end", resolve);
-    })
+    });
   }
 
   function pathOrder(step) {
     if (step == 0) {
-      drawPaths(maderaPaths)
+      drawPaths(maderaPaths);
     } else if (step == 3) {
-      drawPaths(gardenaPaths)
+      drawPaths(gardenaPaths);
     } else if (step == 6) {
-      drawPaths(fremontPaths)
+      drawPaths(fremontPaths);
     }
   }
 
@@ -51,70 +51,77 @@
     gardenaPaths = selectAll("#gardena-streets path");
     fremontPaths = selectAll("#fremont-streets path");
   });
-  </script>
-  
-    <div class:visible="{step >= 0 && step < 3}" id="madera-wrapper">
-      <div class="svg-wrapper">{@html madera}</div>
-      <div class="img-wrapper"><img src="assets/img/madera-streets.png" alt="street grid lines of madera, ca." /></div>
-    </div>
-    <div class:visible="{step > 2 && step < 6}" id="gardena-wrapper">
-      <div class="svg-wrapper">{@html gardena}</div>
-      <div class="img-wrapper"><img src="assets/img/gardena-streets.png" alt="street grid lines of gardena, ca." /></div>
-    </div>
-    <div class:visible="{step >= 6}" id="fremont-wrapper">
-      <div class="svg-wrapper">{@html fremont}</div>
-      <div class="img-wrapper"><img src="assets/img/fremont-streets.png" alt="street grid lines of fremont, ca." /></div>
-    </div>
+</script>
 
-  <style>
-    div {
-      position: absolute;
-      width: 100%;
-      height: 100vh;
-      z-index: 1;
-      opacity: 0;
-      transition: opacity 1s;
-    }
+<div class:visible={step >= 0 && step < 3} id="madera-wrapper">
+  <div class="svg-wrapper">{@html madera}</div>
+  <div class="img-wrapper">
+    <img src="assets/img/madera-streets.png" alt="street grid lines of madera, ca." />
+  </div>
+</div>
+<div class:visible={step > 2 && step < 6} id="gardena-wrapper">
+  <div class="svg-wrapper">{@html gardena}</div>
+  <div class="img-wrapper">
+    <img src="assets/img/gardena-streets.png" alt="street grid lines of gardena, ca." />
+  </div>
+</div>
+<div class:visible={step >= 6} id="fremont-wrapper">
+  <div class="svg-wrapper">{@html fremont}</div>
+  <div class="img-wrapper">
+    <img src="assets/img/fremont-streets.png" alt="street grid lines of fremont, ca." />
+  </div>
+</div>
 
-    .visible {
-      opacity: 1;
-    }
+<style>
+  div {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    z-index: 1;
+    opacity: 0;
+    transition: opacity 1s;
+  }
 
-    .svg-wrapper, .img-wrapper {
-      opacity: 1;
-      width: 100%;
-      height: 100vh;
-    }
+  .visible {
+    opacity: 1;
+  }
 
+  .svg-wrapper,
+  .img-wrapper {
+    opacity: 1;
+    width: 100%;
+    height: 100vh;
+  }
+
+  #madera-wrapper {
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  #gardena-wrapper {
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  #fremont-wrapper {
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  @media only screen and (min-width: 700px) {
     #madera-wrapper {
-      width: 100%;
-      top: 0;
-      left: 0;
+      width: calc(100% - 8rem);
     }
 
     #gardena-wrapper {
-      width: 100%;
-      top: 0;
-      left: 0;
+      width: calc(100% - 8rem);
     }
 
     #fremont-wrapper {
-      width: 100%;
-      top: 0;
-      left: 0;
+      width: calc(100% - 8rem);
     }
-
-    @media only screen and (min-width: 700px) {
-      #madera-wrapper {
-        width: calc(100% - 8rem);
-      }
-
-      #gardena-wrapper {
-        width: calc(100% - 8rem);
-      }
-
-      #fremont-wrapper {
-        width: calc(100% - 8rem);
-      }
-    }
-  </style>
+  }
+</style>
